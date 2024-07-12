355d39e0a640ffe7225bd50f998b9b36be82ac8c
Süleyman Kaya <suleymankayaxyz@gmail.com>
2024-07-12 17:33:31 +0300

    some tests has done.

--- can_manager/transpiler_denemeleri/output.cpp
+++ can_manager/transpiler_denemeleri/output.cpp
@@ -10,73 +10,41 @@ uint64_t extract_bits(uint64_t input, int start, int end) {
     return (input & mask) >> start;
 }
 
-class CAN1 {
+class CAN_Cihazi {
 public:
-    std::bitset<9> output;
-    CAN1() {
+    std::bitset<5> output;
+    CAN_Cihazi() {
         output.reset();
     }
-    std::bitset<5> get_output1() const {
-        return extract_bits<5>(output.to_ullong(), 0, 4);
+    std::bitset<3> get_output1() const {
+        return extract_bits<3>(output.to_ullong(), 0, 2);
     }
-    std::bitset<5> get_output2() const {
-        return extract_bits<5>(output.to_ullong(), 4, 8);
+    std::bitset<3> get_output2() const {
+        return extract_bits<3>(output.to_ullong(), 2, 4);
     }
 };
 
-class CAN2 {
-public:
-    std::bitset<17> output;
-    CAN2() {
-        output.reset();
-    }
-    std::bitset<9> get_output1() const {
-        return extract_bits<9>(output.to_ullong(), 0, 8);
-    }
-    std::bitset<9> get_output2() const {
-        return extract_bits<9>(output.to_ullong(), 8, 16);
-    }
-};
-
-uint64_t MAN1(uint64_t input1, uint64_t input2) {
-    uint64_t output1 = input1 + extract_bits<5>(2, 4, 8);
+uint64_t Bitwise_Sum(uint64_t input1, uint64_t input2) {
+    uint64_t output1 = input1 + input2;
     return output1;
 }
 
-uint64_t MAN2(uint64_t input1) {
-    uint64_t output1 = extract_bits<3>(1, 0, 2);
-    return output1;
-}
+uint64_t Sonuc;
 
-int64_t VAR1;
-uint64_t VAR2;
-uint64_t VAR3;
-
+// Test zamanııı :))
 int main() {
-    CAN1 can1;
-    CAN2 can2;
+    CAN_Cihazi can_cihazi;
 
     // Örnek test verileri
-    can1.output = std::bitset<9>("111111101");
-    can2.output = std::bitset<17>("11011011011011011");  // 112171
+    can_cihazi.output = std::bitset<5>("10101");  // 21
 
-    std::cout << "CAN1 output: " << can1.output << std::endl;
-    std::cout << "CAN1 get_output1: " << can1.get_output1() << std::endl;
-    std::cout << "CAN1 get_output2: " << can1.get_output2() << std::endl;
+    std::cout << "CAN_Cihazi output: " << can_cihazi.output << std::endl;
+    std::cout << "CAN_Cihazi get_output1: " << can_cihazi.get_output1() << std::endl;
+    std::cout << "CAN_Cihazi get_output2: " << can_cihazi.get_output2() << std::endl;
 
-    std::cout << "CAN2 output: " << can2.output << std::endl;
-    std::cout << "CAN2 get_output1: " << can2.get_output1() << std::endl;
-    std::cout << "CAN2 get_output2: " << can2.get_output2() << std::endl;
+    Sonuc = Bitwise_Sum(can_cihazi.get_output1().to_ullong(), can_cihazi.get_output2().to_ullong());
 
-    uint64_t result_MAN1_0 = MAN1(can1.get_output2().to_ullong(), can1.get_output2().to_ullong());
-    uint64_t result_MAN1_1 = MAN1(can2.get_output1().to_ullong(), can2.get_output1().to_ullong());
-    VAR1 = result_MAN1_0;
-
-    std::cout << "Result MAN1 (CAN1 output2): " << result_MAN1_0 << std::endl;
-    std::cout << "Result MAN1 (CAN2 output1): " << result_MAN1_1 << std::endl;
-
-    uint64_t result_MAN2_0 = MAN2(can1.get_output1().to_ullong());
-    std::cout << "Result MAN2 (CAN1 output1): " << result_MAN2_0 << std::endl;
+    std::cout << "Result Bitwise_Sum (Sonuc): " << Sonuc << std::endl;
 
     return 0;
 }
